e232072465c4a092e2d246cf02652af9499bf589
yfabillah <yudith@upi.edu>
2022-09-12 16:59:21 +0700

    first commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+})
+
+connection.connect(error => {
+    if(error){
+        throw error
+    }
+
+    console.log(`Successfully connected to database: ${process.env.DATABASE}`)
+})
+
+module.exports = connection
